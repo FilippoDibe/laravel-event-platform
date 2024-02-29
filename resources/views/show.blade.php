fd779adf1629d3b39eac1d5d2ea90a34e2f77aec
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-29 16:14:25 +0100

    add css

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,13 +2,7 @@
 @section('content')
 
 <div class="title_container">
-    <h1 class="title">STAMPA DEGLI EVENTI</h1>
-</div>
-
-<div class="button_container">
-    <button class="ms_button">
-        <a href="#">AGGIUNGI NUOVO EVENTO</a>
-    </button>
+    <h1 class="title">INFORMAZIONI SULL'EVENTO</h1>
 </div>
 
 <div class="col_container">
@@ -17,9 +11,15 @@
 
             {{-- Le tre voci qui sotto sono solo a titolo esemplificativo --}}
             <span>Nome dell'evento: </span>
+            <span>Descrizione dell'evento: </span>
             <span>Data dell'evento: </span>
-
         </div>
     </div>
+</div>
+
+<div class="button_container">
+    <button class="ms_button">
+        <a href="#">TORNA ALLA PAGINA PRINCIPALE</a>
+    </button>
 </div>
 @endsection
