a271de2597786ad3fc3963b9869f9298f71d4c6f
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-03-01 15:38:24 +0100

    add front end api comunication

---
+++ front-end/vue-event-platform/src/components/ApiContent.vue
@@ -0,0 +1,28 @@
+<script>
+import  axios  from 'axios';
+export default{
+    name: 'ApiContent',
+    mounted() {
+        console.log('app caricata');
+
+        axios.get('http://localhost:8000/api/v1/test')
+        .then(res=>{
+            const data = res.data;
+            console.log(data);
+        })
+        .catch((err)=>{console.err(err)});
+
+
+
+    }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
